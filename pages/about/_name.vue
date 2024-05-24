37b530a45d0103294842d866083bdf2281279327
root <root@DESKTOP-0CCAVJM.>
2024-05-24 04:26:25 +0000

    First commit

---
+++ pages/about/_name.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="about">
+      <h1>Hello {{ their_name }}, this is an about page</h1>
+      <p>BTVN Frontend - Vuejs</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'about',
+    data() {
+        return {
+            their_name: this.$route.params.name
+        }
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top: 50px;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Do Hoang Son</p>
+  </div>
+</template>
+
+<script>
+</script>
